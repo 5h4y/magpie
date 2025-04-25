f8b5c9ea5cdee860c5a717d2e1a0e00fb79e59b7
shay <32275182+404shay@users.noreply.github.com>
2025-04-24 21:53:05 -0400

    updated space; added standard SI units for acceleration to response

--- science/gravity_acceleration.py
+++ science/gravity_acceleration.py
@@ -2,8 +2,8 @@ G = 6.673e-11
 M = 5.98e24
 accel_gravity = 0.0
 
-dist_center = float(input("What is the distance in meters from the Earth's center?"))
+dist_center = float(input("What is the distance in meters from the Earth's center?: "))
 
 accel_gravity = (G * M) / dist_center ** 2
 
-print(f'Acceleration of gravity: {accel_gravity:.2f}')
+print(f'Acceleration of gravity: {accel_gravity:.2f} m/s²')
